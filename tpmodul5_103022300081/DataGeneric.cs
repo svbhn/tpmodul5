1cfcc4969f364b14a36f5dcd0f753107643c1cc4
unknown <subhanmaualana14@gmail.com>
2025-03-16 18:44:33 +0700

    menambahkan class DataGeneric

---
+++ tpmodul5_103022300081/DataGeneric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace tpmodul5_103022300081
+{
+    class DataGeneric<T>
+    {
+        private T Data { get; set; }
+
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+    }
+}
--- tpmodul5_103022300081/obj/Debug/net8.0/tpmodul5_103022300081.AssemblyInfo.cs
+++ tpmodul5_103022300081/obj/Debug/net8.0/tpmodul5_103022300081.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_103022300081")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0835a918a7ee3d6725214741bb740f05e7ecdc30")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_103022300081")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_103022300081")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
